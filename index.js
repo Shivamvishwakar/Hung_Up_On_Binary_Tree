5626dd57a402eaea93855b711711fac352e94f92
Shivam Vishwakarma <sv8269514494shivam@gmail.com>
2023-11-04 11:02:34 +0530

    Third Commit

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ document.getElementById('submit-issue-button').addEventListener('click', functio
 // Implement code to fetch and display real-time updates from city departments
 
 // Implement code to populate the user dashboard with user-specific data
+
+
+// issue page js 
+// script.js
+
+// Basic form validation
+document.querySelector("form").addEventListener("submit", function (event) {
+    const issueType = document.querySelector("#issue_type");
+    const description = document.querySelector("#description");
+    const location = document.querySelector("#location");
+
+    if (!issueType.value || !description.value || !location.value) {
+        alert("Please fill in all required fields.");
+        event.preventDefault();
+    }
+});
